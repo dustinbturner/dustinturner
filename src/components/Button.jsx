68cd81446d7ba606da1e6f9d01447e0e70a5ad2c
Dustin Turner <dustin@dustinbturner.com>
2024-03-08 17:01:27 -0500

    new portfolio testing

---
+++ src/components/Button.jsx
@@ -0,0 +1,74 @@
+import Link from 'next/link'
+import clsx from 'clsx'
+
+function ArrowIcon(props) {
+  return (
+    <svg viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}>
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"
+      />
+    </svg>
+  )
+}
+
+const variantStyles = {
+  primary:
+    'rounded-md bg-neutral-900 py-1 px-3 text-white hover:bg-neutral-700 dark:bg-green-400/10 dark:text-green-400 dark:ring-1 dark:ring-inset dark:ring-green-400/20 dark:hover:bg-green-400/10 dark:hover:text-green-300 dark:hover:ring-green-300',
+  secondary:
+    'rounded-md bg-neutral-100 py-1 px-3 text-neutral-900 hover:bg-neutral-200 dark:bg-neutral-800/40 dark:text-neutral-400 dark:ring-1 dark:ring-inset dark:ring-neutral-800 dark:hover:bg-neutral-800 dark:hover:text-neutral-300',
+  filled:
+    'rounded-md bg-neutral-900 py-1 px-3 text-white hover:bg-neutral-700 dark:bg-green-500 dark:text-white dark:hover:bg-green-400',
+  outline:
+    'rounded-md py-1 px-3 text-neutral-700 ring-1 ring-inset ring-neutral-900/10 hover:bg-neutral-900/2.5 hover:text-neutral-900 dark:text-neutral-400 dark:ring-white/10 dark:hover:bg-white/5 dark:hover:text-white',
+  text: 'text-green-500 hover:text-green-600 dark:text-green-400 dark:hover:text-green-500',
+}
+
+export function Button({
+  variant = 'primary',
+  className,
+  children,
+  arrow,
+  ...props
+}) {
+  className = clsx(
+    'inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition',
+    variantStyles[variant],
+    className,
+  )
+
+  let arrowIcon = (
+    <ArrowIcon
+      className={clsx(
+        'mt-0.5 h-5 w-5',
+        variant === 'text' && 'relative top-px',
+        arrow === 'left' && '-ml-1 rotate-180',
+        arrow === 'right' && '-mr-1',
+      )}
+    />
+  )
+
+  let inner = (
+    <>
+      {arrow === 'left' && arrowIcon}
+      {children}
+      {arrow === 'right' && arrowIcon}
+    </>
+  )
+
+  if (typeof props.href === 'undefined') {
+    return (
+      <button className={className} {...props}>
+        {inner}
+      </button>
+    )
+  }
+
+  return (
+    <Link className={className} {...props}>
+      {inner}
+    </Link>
+  )
+}
